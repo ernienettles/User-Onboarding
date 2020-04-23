c97f24f818ee98ef2d13f3469bcf6561f056ca39
Ernie Nettles <ernienettles@gmail.com>
2020-04-22 20:07:28 -0400

    MVP finished

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -13,10 +13,10 @@ We want to create a form to onboard a new user to our system. We need at least t
  */
 
     const formSchema = yup.object().shape({
-        name: yup.string().required(),
-        email: yup.string().email(),
-        password: yup.string().required(),
-        terms: yup.boolean().required(),
+        name: yup.string().required("Name is a required field."),
+        email: yup.string().email("Must be a valid email address.").required("Must include an email address."),
+        password: yup.string().required("Choose your password."),
+        terms: yup.boolean().oneOf([true], "You must accept the Terms & Conditions."),
     })
 
 
@@ -29,18 +29,74 @@ We want to create a form to onboard a new user to our system. We need at least t
             terms: ""
         });
 
+        const [errors, setErrors] = useState({
+            name: "",
+            email: "",
+            password: "",
+            terms: ""
+        });
 
+        const [buttonDisabled, setButtonDisabled] = useState(true);
 
+        const [users, setUsers] = useState([]);
+
+        const formSubmit = e => {
+            e.preventDefault();
+            axios.post("https://reqres.in/api/users", formState)
+            .then(response => {
+                setUsers(response.data);
+                console.log("Success!", users);
+                setFormState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    terms: ""
+                })
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
+        }
+
+        useEffect(() => {
+            formSchema.isValid(formState).then(valid => {
+                setButtonDisabled(!valid);
+            })
+        }, [formState]);
+
+        const validateChange = e => {
+            yup.reach(formSchema, e.target.name).validate(e.target.value)
+            .then(valid => {
+                setErrors({ ...errors, [e.target.name]: ""})
+            })
+            .catch(error => {
+                setErrors({
+                    ...errors, [e.target.name]: error.errors[0]
+                })
+            });
+        };
+
+        const inputChange = e => {
+            e.persist();
+            const newFormData = {
+                ...formState, [e.target.name]: e.target.type === "checkbox" ? e.target.checked : e.target.value
+            };
+            validateChange(e);
+            setFormState(newFormData);
+        };
 
         return (
-            <form>
+            <form onSubmit={formSubmit}>
                 <label htmlFor="name">
                     Name
                     <input 
                     type="text"
                     name="name"
                     placeholder="Name"
+                    value={formState.name}
+                    onChange={inputChange}
                     />
+                    {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
                 </label>
                 <label htmlFor="email">
                     Email
@@ -48,7 +104,10 @@ We want to create a form to onboard a new user to our system. We need at least t
                     type="email"
                     name="email"
                     placeholder="Email"
+                    value={formState.email}
+                    onChange={inputChange}
                     />
+                    {errors.email.length > 0 ? <p className="error">{errors.email}</p> : null}
                 </label>
                 <label htmlFor="password">
                     Password
@@ -56,17 +115,22 @@ We want to create a form to onboard a new user to our system. We need at least t
                     type="password"
                     name="password"
                     placeholder="Password"
+                    value={formState.password}
+                    onChange={inputChange}
                     />
+                    {errors.password.length > 0 ? <p className="error">{errors.password}</p> : null}
                 </label>
                 <label htmlFor="terms" className="terms">
                     <input
                     type="checkbox"
                     name="terms"
-                    checked={true}
+                    checked={formState.terms}
+                    onChange={inputChange}
                     />
                     Terms & Conditions
                 </label>
-                <button type="submit">Submit</button>
+                <button disabled={buttonDisabled}>Submit</button>
+                <pre>{JSON.stringify(users, null, 2)}</pre>
             </form>
             
         )
