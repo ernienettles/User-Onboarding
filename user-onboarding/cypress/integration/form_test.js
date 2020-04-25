ae00a548067da7dbfbdf32cc75c3451f3bf64683
Ernie Nettles <ernienettles@gmail.com>
2020-04-24 22:53:08 -0400

    Fixed test

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -19,21 +19,18 @@ describe("Test our inputs and submit our form", function()
         cy.get('button')
             .click()
     })
-})
 
-
-describe("Validation test", function() 
-{
-    beforeEach(function() {
-        cy.visit("http://localhost:3000/");
+    it("Check for form validation.", function() {
+        cy.get('input[name="email"]')
+            .type('ernie@ernie.com')
+            .should("have.value", "ernie@ernie.com");
+        cy.get('input[name="password"]')
+            .type('password')
+            .should("have.value", "password");
+        cy.get('[type="checkbox"]')
+            .check()
+            .should("be.checked");
+        cy.get('button')
+            .click()
     })
-    it('Checking for validation', () => {
-        cy.get('input[name="name"]').type('Ernie').should('have.length', 1)
-        cy.get('input[name="email"]').type('not_an_email')
-        cy.get('input[name="password"]').type('password').should('have.length', 1)
-        cy.get('[type="checkbox"]').check().should("be.checked")
-        cy.get('input[name="email"]').then(($input) => {
-          expect($input[0].validationMessage).to.eq('I expect an email!')
-        })
-      })
 })
